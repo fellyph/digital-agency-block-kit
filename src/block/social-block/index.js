c7b9fd63e36d8b582d854566f494e89dc8e5a291
fellyph <fellyph.cintra@gmail.com>
2019-02-17 09:48:48 +0000

    adding insta block

---
+++ src/block/social-block/index.js
@@ -0,0 +1,58 @@
+//  Import CSS.
+import './style.scss';
+import './editor.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const {
+	TextControl,
+	PanelBody,
+	PanelRow,
+} = wp.components;
+const { Fragment } = wp.element;
+
+registerBlockType( 'cgb/block-products', {
+
+	title: __( 'Social Card' ),
+	icon: 'shield',
+	category: 'common',
+	keywords: [
+		__( 'Social' ),
+		__( 'Social media' ),
+		__( 'Instagram' ),
+	],
+
+	attributes: {
+		instagramUser: {
+			type: 'string',
+			default: 'fellyph',
+		},
+	},
+
+	edit: ( { attributes, setAttributes } ) => {
+		const { instagramUser } = attributes;
+
+		return (
+			<Fragment>
+				<PanelBody title={ __( 'InstaBlock' ) } initialOpen={ true } >
+					<PanelRow>
+						<TextControl
+							value={ instagramUser }
+							onChange={ newUser => setAttributes( { instagramUser: newUser } ) }
+							label={ __( 'Add your instagram user here' ) } />
+					</PanelRow>
+				</PanelBody>
+			</Fragment>
+		);
+	},
+
+	save: ( { attributes } ) => {
+		const { instagramUser } = attributes;
+		return (
+			<div data-user={ instagramUser }>
+				<h3>{ __( 'Instagram user' ) }</h3>
+				<a href={ 'http://instagram.com/' + instagramUser }> { instagramUser } </a>
+			</div>
+		);
+	},
+} );
