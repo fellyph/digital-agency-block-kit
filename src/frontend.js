74e260ec6db11bd85c6d1a06ddd75aae8bedec6f
fellyph <fellyph.cintra@gmail.com>
2019-02-19 00:47:12 +0000

    updating webpack.config

---
+++ src/frontend.js
@@ -0,0 +1,1 @@
+import './block/3-testimonials';
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,68 @@
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require( 'path' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
-const blocksCSSPlugin = new ExtractTextPlugin({
-  filename: "./dist/css/blocks.style.css"
-});
-const editBlocksCSSPlugin = new ExtractTextPlugin({
-  filename: "./dist/css/blocks.editor.css"
-});
-const commonCSSPlugin = new ExtractTextPlugin({
-  filename: "./dist/css/common.css"
-});
+const blocksCSSPlugin = new ExtractTextPlugin( {
+	filename: './dist/blocks.style.build.css',
+} );
+const editBlocksCSSPlugin = new ExtractTextPlugin( {
+	filename: './dist/blocks.editor.build.css',
+} );
 
 // Configuration for the ExtractTextPlugin.
 const extractConfig = {
-  use: [
-    { loader: "raw-loader" },
-    {
-      loader: "postcss-loader",
-      options: {
-        plugins: [require("autoprefixer")]
-      }
-    },
-    {
-      loader: "sass-loader",
-      query: {
-        outputStyle:
-          "production" === process.env.NODE_ENV ? "compressed" : "nested"
-      }
-    }
-  ]
+	use: [ {
+		loader: 'raw-loader'
+	},
+	{
+		loader: 'postcss-loader',
+		options: {
+			plugins: [ require('autoprefixer') ],
+		},
+	},
+	{
+		loader: 'sass-loader',
+		query: {
+			outputStyle: 'compressed',
+		},
+	} ],
 };
 
 module.exports = {
-  entry: {
-    "./assets/js/blocks.editor": "./blocks/blocks.js",
-    "./assets/js/blocks.frontend": "./blocks/frontend.js"
-  },
-  output: {
-    path: path.resolve(__dirname),
-    filename: "[name].js"
-  },
-  externals: {
-    react: "React", // React itself is there in Gutenberg.
-		jquery: "jQuery", // import $ from 'jquery' // Use the WordPress version after enqueuing it.
-		"react-dom": "ReactDOM",
-  },
-  watch: "production" !== process.env.NODE_ENV,
-  devtool: "cheap-module-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /style\.s?css$/,
-        use: blocksCSSPlugin.extract(extractConfig)
-      },
-      {
-        test: /editor\.s?css$/,
-        use: editBlocksCSSPlugin.extract(extractConfig)
-      },
-      {
-        test: /common\.s?css$/,
-        use: commonCSSPlugin.extract(extractConfig)
-      }
-    ]
-  },
-  plugins: [blocksCSSPlugin, editBlocksCSSPlugin, commonCSSPlugin]
+	entry: {
+		'./assets/js/blocks.editor': './blocks/blocks.js',
+		'./assets/js/blocks.frontend': './blocks/frontend.js',
+	},
+	output: {
+		path: path.resolve(__dirname),
+		filename: '[name].js'
+	},
+	externals: {
+		react: 'React', // React itself is there in Gutenberg.
+		jquery: 'jQuery', // import $ from 'jquery' // Use the WordPress version after enqueuing it.
+		'react-dom': 'ReactDOM',
+	},
+	watch: 'production' !== process.env.NODE_ENV,
+	devtool: 'cheap-module-source-map',
+	module: {
+		rules: [ {
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+			},
+		},
+		{
+			test: /style\.s?css$/,
+			use: blocksCSSPlugin.extract(extractConfig),
+		},
+		{
+			test: /editor\.s?css$/,
+			use: editBlocksCSSPlugin.extract(extractConfig),
+		},
+		{
+			test: /common\.s?css$/,
+			use: commonCSSPlugin.extract(extractConfig),
+		} ],
+	},
+	plugins: [blocksCSSPlugin, editBlocksCSSPlugin, commonCSSPlugin]
 };
